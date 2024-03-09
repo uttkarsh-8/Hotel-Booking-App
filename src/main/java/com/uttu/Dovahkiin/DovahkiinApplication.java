5d3d743fa3514ac006116e1dd14dfbe00c1bf434
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-03-10 00:41:34 +0530

    First commit

---
+++ src/main/java/com/uttu/Dovahkiin/DovahkiinApplication.java
@@ -0,0 +1,13 @@
+package com.uttu.Dovahkiin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DovahkiinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DovahkiinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uttu/Dovahkiin/DovahkiinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttu.Dovahkiin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DovahkiinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
